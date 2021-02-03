ff8fc0466a96b0fecc718a42329fbbbd30e8dfe1
Taylor Vance <tvance@engage2excel.com>
2021-02-02 20:17:35 -0600

    Banner create and edit views

--- banners/urls.py
+++ banners/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.BannerList.as_view(), name='banner-list'),
     path('<int:pk>/', views.BannerView.as_view(), name='banner-view'),
     path('<int:pk>/edit/', views.BannerEdit.as_view(), name='banner-edit'),
+    path('create/', views.BannerCreate.as_view(), name='banner-create'),
 
     path('<int:pk>/reserve/', views.reserve_slot, name='reserve-slot'),
 ]
--- banners/views.py
+++ banners/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.utils import timezone
 
 from .models import Banner, Slot
@@ -10,13 +11,34 @@ class BannerList(ListView):
     queryset = Banner.objects.filter(end_at__gt=timezone.now())
     template_name = 'banners/banner_list.html'
 
+
+class BannerCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'is_staff'
+    model = Banner
+    template_name = 'banners/banner_create.html'
+    fields = ['name', 'start_at', 'end_at', 'administrator', 'moderator', 'staff', 'participants', 'url']
+    success_url = '/'
+
+    def get_form_kwargs(self):
+        kwargs = super(BannerCreate, self).get_form_kwargs()
+        if kwargs['instance'] is None:
+            kwargs['instance'] = Banner()
+        kwargs['instance'].author = self.request.user
+        return kwargs
+
+
+class BannerEdit(PermissionRequiredMixin, UpdateView):
+    permission_required = 'is_staff'
+    model = Banner
+    template_name = 'banners/banner_edit.html'
+    fields = ['name', 'start_at', 'end_at', 'administrator', 'moderator', 'staff', 'participants', 'url']
+    success_url = '/'
+
+
 class BannerView(DetailView):
     model = Banner
     template_name = 'banners/banner_view.html'
 
-class BannerEdit(DetailView):
-    model = Banner
-    template_name = 'banners/banner_edit.html'
 
 @login_required
 def reserve_slot(request, pk):
