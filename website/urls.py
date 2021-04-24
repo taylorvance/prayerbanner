0e5c27eb208b644f14387e0f52aff4890017a032
Taylor Vance <tmax8908@yahoo.com>
2021-04-24 14:21:08 -0500

    FAQ

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
-# from . import views
 from banners import views as banner_views
 
 urlpatterns = [
     path('', banner_views.BannerList.as_view(), name='banner-list'),
+    path('faq/', TemplateView.as_view(template_name='faq.html'), name='faq'),
 ]
