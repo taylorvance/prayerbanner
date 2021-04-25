a82f50963fea16496360a3b51b7696818a156b45
Taylor Vance <tmax8908@yahoo.com>
2021-04-25 14:59:35 -0500

    Tweak order

--- banners/admin.py
+++ banners/admin.py
@@ -6,7 +6,7 @@ class BannerAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'start_at', 'end_at', 'administrator')
 
 class SlotAdmin(admin.ModelAdmin):
-    list_display = ('id', 'banner', 'user', 'start_at', 'end_at', 'reminder_sent')
+    list_display = ('id', 'banner', 'start_at', 'end_at', 'user', 'reminder_sent')
 
 admin.site.register(Banner, BannerAdmin)
 admin.site.register(Slot, SlotAdmin)
