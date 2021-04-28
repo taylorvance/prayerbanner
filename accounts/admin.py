3c71b5a735e44387e0239b7363e19981085aac6c
Taylor Vance <tmax8908@yahoo.com>
2021-04-28 10:40:35 -0500

    Tweak capcha and show last login in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name', 'church', 'weekend', 'timezone')
+    list_display = ('id', 'email', 'first_name', 'last_name', 'church', 'weekend', 'timezone', 'last_login')
 
 admin.site.register(User, UserAdmin)
